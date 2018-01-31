88c720f3af860ce57bfda7e5dc90b08675548ae7
bovandersteene <bo@reibo.io>
2018-01-31 10:35:01 +0100

    Adjust jest config file to have a 100% Test coverage when covered

--- jest.config.js
+++ jest.config.js
@@ -1,39 +1,49 @@
 module.exports = {
-    "globals": {
-        "ts-jest": {
-            "tsConfigFile": "src/tsconfig.spec.json"
-        },
-        "__TRANSFORM_HTML__": true
+  mapCoverage: true,
+  globals: {
+    '__TS_CONFIG__': {
+      'target': 'es6',
+      'module': 'commonjs',
+      'moduleResolution': 'node'
     },
-    "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|js)$",
-    "setupTestFrameworkScriptFile": "<rootDir>/src/setupJest.ts",
-    "transform": {
-        "^.+\\.(ts|html)$": "<rootDir>/node_modules/jest-preset-angular/preprocessor.js",
+    'ts-jest': {
+      'tsConfigFile': 'src/tsconfig.spec.json'
     },
-    "transformIgnorePatterns": [
-        "node_modules/(?!@ngrx)"
-    ],
-    "collectCoverageFrom": [
-        "src/app/**/*.{ts}",
-        "!src/app/**/*.{js}",
-        "!src/app/environment/*.{ts}",
-        "!src/app/**/*.module.{ts}",
-        "!src/app/**/*.state.{ts}",
-        "!src/app/**/*.entity.{ts}"
-    ],
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "json"
-    ],
-    testPathIgnorePatterns: [
-        '/node_modules/',
-        "src/app/*.{js}",
-    ],
-    "testResultsProcessor": "jest-sonar-reporter",
-    "moduleNameMapper": {
-        "app/(.*)": "<rootDir>/src/app/$1",
-        "@common/(.*)": "<rootDir>/src/app/common/$1",
-    }
+    '__TRANSFORM_HTML__': true
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|js)$',
+  setupTestFrameworkScriptFile: '<rootDir>/src/setupJest.ts',
+  transform: {
+    '^.+\\.(ts|html)$': '<rootDir>/node_modules/jest-preset-angular/preprocessor.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!@ngrx)'
+  ],
+  collectCoverageFrom: [
+    'src/app/module/**/*.{ts}',
+    '!src/app/*.{ts}',
+    '!src/app/**/*.{js}',
+    '!src/app/environment/*.{ts}',
+    '!src/app/language/*.{ts}',
+    '!src/app/**/*.module.{ts}',
+    '!src/app/**/*.interface.{ts}',
+    '!src/app/**/*.state.{ts}',
+    '!src/app/**/*.entity.{ts}'
+  ],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'json'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    'src/app/*.{js}'
+  ],
+  testResultsProcessor: 'jest-sonar-reporter',
+  moduleNameMapper: {
+    "app/(.*)": "<rootDir>/src/app/$1",
+    "@common/(.*)": "<rootDir>/src/app/common/$1",
+  }
 };
